ac8aca7dbb9fca1ccc9372462db2b31f30d50a84
dhananjaybhatia <dhananjaybhatia@yahoo.co.in>
2025-07-16 21:04:55 +1000

    maxElemts with binary search style

--- Arrays/maxElems.js
+++ Arrays/maxElems.js
@@ -1,19 +1,57 @@
-function maxElems(arr) {
-  let pos = 0;
-  let neg = 0;
-  let max = 0;
+// function maxElems(arr) {
+//   let pos = 0;
+//   let neg = 0;
+//   let max = 0;
 
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] < 0) {
-      neg++;
-    } else if (arr[i] > 0) {
-      pos++;
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] < 0) {
+//       neg++;
+//     } else if (arr[i] > 0) {
+//       pos++;
+//     }
+//     max = Math.max(pos, neg);
+//   }
+//   return max;
+// }
+
+// arr = [-3, -2, -1, 0, 1, 2, 3, 4];
+
+// console.log(maxElems(arr));
+
+function maxNums(arr) {
+  const n = arr.length;
+
+  let left = 0,
+    right = n - 1,
+    firstPos = 0;
+  while (left <= right) {
+    let mid = Math.ceil((left + right) / 2); //mid is 4 now
+    //arr[mid] is 1
+    if (arr[mid] > 0) {
+      firstPos = mid; //firstPos is 4 now
+      right = mid - 1; //right is 3 now
+    } else {
+      left = mid + 1; //left is 5 now
     }
-    max = Math.max(pos, neg);
   }
-  return max;
+
+  (left = 0), (right = n - 1), (lastNeg = -1);
+
+  while (left <= right) {
+    let mid = Math.ceil((left + right) / 2); //mid is 4 now
+    if (arr[mid] < 0) {
+      lastNeg = mid;
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  const posCount = n - firstPos;
+  const negCount = lastNeg + 1;
+
+  return Math.max(posCount, negCount);
 }
 
 arr = [-3, -2, -1, 0, 1, 2, 3, 4];
 
-console.log(maxElems(arr));
+console.log(maxNums(arr));
