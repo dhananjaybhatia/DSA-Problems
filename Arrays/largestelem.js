f2dd0a77f9dba9965e4fcba175849f72c58eca9c
dhananjaybhatia <dhananjaybhatia@yahoo.co.in>
2025-07-20 12:55:50 +1000

    largestElem

---
+++ Arrays/largestelem.js
@@ -0,0 +1,15 @@
+//Find the largest element in the array.
+
+function largestElem(arr) {
+  if (!arr.length) return false;
+  largest = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > largest) {
+      largest = arr[i];
+    }
+  }
+  return largest;
+}
+
+arr = [3, 2, 5, 1, 7, 2];
+console.log(largestElem(arr));
